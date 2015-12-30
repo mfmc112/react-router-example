46daf8a3db4f06a54d0bb4c70060dc3cd8d59c38
Marcos Costa <mfmc112@gmail.com>
2015-12-30 14:54:04 -0600

    Update routes.js

--- src/routes.js
+++ src/routes.js
@@ -7,12 +7,15 @@ var Route = ReactRouter.Route;
 var IndexRoute  = ReactRouter.IndexRoute;
 
 var routes = (
-  <Route path="/" component={require('./components/app')}>
-    <IndexRoute component={require('./components/home')}/>
-    <Route path="/home" component={require('./components/home')}/>
-    <Route path="/user" component={require('./components/user')}/>
-    <Route path="/user/:userID" component={require('./components/userById')}/>
-    <Route path="/about" component={require('./components/about')}/>
+  <Route path="/" component={require('./components/App')}>
+    <IndexRoute component={require('./components/Home')}/>
+    <Route path="/home" component={require('./components/Home')}/>
+    <Route path="/users">
+      <IndexRoute path="/" component={require('./components/Users')}/>
+      <Route path=":userID" component={require('./components/User')}/>
+      <Route path="*" component={require('./components/NotFound')}/>
+    </Route>
+    <Route path="/about" component={require('./components/About')}/>
   </Route>
 );
 
