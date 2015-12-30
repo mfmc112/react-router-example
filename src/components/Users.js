244ad987a015fc009c1eb841c28ef608b5f01f99
Marcos Costa <mfmc112@gmail.com>
2015-12-30 14:58:13 -0600

    Update Users.js

--- src/components/Users.js
+++ src/components/Users.js
@@ -4,8 +4,9 @@ var User = React.createClass({
   render: function(){
     return (
       <div className="ui message">
-        <h1 className="ui header">User Screen!</h1>
-        <p>User screen routed by react-router</p>
+        <h1 className="ui header">User screen by ID ({this.props.params.userID}) ! </h1>
+        <p>(This is coming from the URL and routed as userID<br/>
+            User screen by ID routed by react-router</p>
       </div>
     );
   }
