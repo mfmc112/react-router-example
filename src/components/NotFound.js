a38044811939b58e25513113a7ff9221bac81923
Marcos Costa <mfmc112@gmail.com>
2015-12-30 14:58:53 -0600

    Create NotFound.js

---
+++ src/components/NotFound.js
@@ -0,0 +1,11 @@
+var React = require('react');
+
+var NotFound = React.createClass({
+    render: function(){
+        return (
+            <div>404 - Page not found.</div>
+        );
+    }
+});
+
+module.exports = NotFound;
